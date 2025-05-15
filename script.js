64f54eb371afb8fc79c1a915ca69189fadc4c3c1
Okow0714 <ganzorig2003@gmail.com>
2025-05-15 19:11:54 +0900

    File upload 1

---
+++ script.js
@@ -0,0 +1,33 @@
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+  if (!event.target.matches('.text-1') && !event.target.closest('#myDescription')) {
+    var descriptions = document.getElementsByClassName("mainbody-description");
+    var i;
+    for (i = 0; i < descriptions.length; i++) {
+      var openDesc = descriptions[i];
+      if (openDesc.classList.contains('showdescription')) {
+        openDesc.classList.remove('showdescription');
+      }
+    }
+  }
+}
+function myFunctionDescription() {
+  document.getElementById("myDescription").classList.toggle("showdescription");
+}
+
+
+
